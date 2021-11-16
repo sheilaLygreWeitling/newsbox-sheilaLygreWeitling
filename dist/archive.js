4af5164395dfd6e75c1038bcc8487436213371a2
sheilaLygreWeitling <U2407194@rts-365.dk>
2021-11-16 21:07:28 +0100

    Add changes to WU06

--- dist/archive.js
+++ dist/archive.js
@@ -1,1 +1,28 @@
 "use strict";
+
+axios.get("https://api.nytimes.com/svc/topstories/v2/science.json?api-key=RJ9oWjSESWwzZYmsAw6r1GxXh2G8uh7F").then(function (response) {
+  var news = [];
+  news.forEach(function (newsElement) {
+    var newsMain = document.querySelector(".Main");
+  });
+});
+/*
+newsSection.innerHTML += `
+            <section class="Main__section Section">
+        <article class="Section__article">
+            <div class="Section__article-div">
+                <div class="Div">
+                    <img src="/images/newsSectionIcon.png" alt="health">
+                </div>
+                <div class="Div">
+                    <h1>HEALTH</h1>
+                </div>
+                <div class="Div__dropDown">
+                    <button class="Div__dropDown-button">
+                        <i class="fas fa-chevron-down"></i></button>
+                    </button>
+                </div>
+            </div>
+        </article>
+    </section>
+        ` */
---
+++ js/archive.js
@@ -0,0 +1,36 @@
+
+
+axios.get("https://api.nytimes.com/svc/topstories/v2/science.json?api-key=RJ9oWjSESWwzZYmsAw6r1GxXh2G8uh7F").then((response) => {
+    const news = [];
+
+    news.forEach(newsElement => {
+        const newsMain = document.querySelector(".Main")
+
+
+    });
+
+
+
+});
+
+
+/*
+newsSection.innerHTML += `
+            <section class="Main__section Section">
+        <article class="Section__article">
+            <div class="Section__article-div">
+                <div class="Div">
+                    <img src="/images/newsSectionIcon.png" alt="health">
+                </div>
+                <div class="Div">
+                    <h1>HEALTH</h1>
+                </div>
+                <div class="Div__dropDown">
+                    <button class="Div__dropDown-button">
+                        <i class="fas fa-chevron-down"></i></button>
+                    </button>
+                </div>
+            </div>
+        </article>
+    </section>
+        ` */
