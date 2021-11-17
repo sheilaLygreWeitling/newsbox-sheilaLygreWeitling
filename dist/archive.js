4d00a88991b7c1b981cb8048b17955cda3f16dd9
sheilaLygreWeitling <U2407194@rts-365.dk>
2021-11-17 12:18:47 +0100

    Add changes to WU06

--- dist/archive.js
+++ dist/archive.js
@@ -1,12 +1,40 @@
 "use strict";
 
+var myAPIKey = "RJ9oWjSESWwzZYmsAw6r1GxXh2G8uh7F";
+var url = "https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=" + myAPIKey;
 axios.get("https://api.nytimes.com/svc/topstories/v2/science.json?api-key=RJ9oWjSESWwzZYmsAw6r1GxXh2G8uh7F").then(function (response) {
   var news = [];
   news.forEach(function (newsElement) {
-    var newsMain = document.querySelector(".Main");
+    var main = document.querySelector(".Main");
+    var frontPageNews = document.createTextNode(newsElement.response);
+    var section = document.createElement("section");
+    section.classList.add("Section");
+    var article = document.createElement("article");
+    article.classList.add("Section__article");
+    var divSectionArticleDiv = document.createElement("div");
+    divSectionArticleDiv.classList.add("Section__article-div");
+    var div = document.createElement("div");
+    div.classList.add("Div");
+    var headLine = document.createElement("h1");
+    headLine.classList.add("headLine-l");
+    var dropDownDiv = document.createElement("div");
+    dropDownDiv.classList.add("Div__dropDown");
+    var dropDownButton = document.createElement("button");
+    dropDownButton.classList.add("Div__dropDown-button");
+    var dropDownIcon = document.createElement("i");
+    dropDownIcon.classList.add("fas", "fa-chevron-down");
+    main.appendChild(section);
+    section.appendChild(article);
+    article.appendChild(divSectionArticleDiv);
+    article.appendChild(div);
+    div.appendChild(headLine);
+    headLine.appendChild(frontPageNews);
+    article.appendChild(dropDownDiv);
+    dropDownDiv.appendChild(dropDownButton);
+    dropDownButton.appendChild(dropDownIcon);
   });
 });
-/*
+/* const newsSection = document.querySelector(".Main")
 newsSection.innerHTML += `
             <section class="Main__section Section">
         <article class="Section__article">
@@ -25,4 +53,5 @@ newsSection.innerHTML += `
             </div>
         </article>
     </section>
-        ` */
+        `
+ */
--- js/archive.js
+++ js/archive.js
@@ -1,20 +1,53 @@
+let myAPIKey = "RJ9oWjSESWwzZYmsAw6r1GxXh2G8uh7F";
+let url = "https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=" + myAPIKey
 
+axios.get("https://api.nytimes.com/svc/topstories/v2/science.json?api-key=RJ9oWjSESWwzZYmsAw6r1GxXh2G8uh7F")
+    .then((response) => {
+        const news = [];
 
-axios.get("https://api.nytimes.com/svc/topstories/v2/science.json?api-key=RJ9oWjSESWwzZYmsAw6r1GxXh2G8uh7F").then((response) => {
-    const news = [];
+        news.forEach(newsElement => {
+            const main = document.querySelector(".Main")
 
-    news.forEach(newsElement => {
-        const newsMain = document.querySelector(".Main")
+            let frontPageNews = document.createTextNode(newsElement.response);
 
+            let section = document.createElement("section")
+            section.classList.add("Section")
 
+            let article = document.createElement("article")
+            article.classList.add("Section__article")
+
+            let divSectionArticleDiv = document.createElement("div")
+            divSectionArticleDiv.classList.add("Section__article-div")
+
+            let div = document.createElement("div")
+            div.classList.add("Div")
+
+            let headLine = document.createElement("h1")
+            headLine.classList.add("headLine-l")
+
+            let dropDownDiv = document.createElement("div")
+            dropDownDiv.classList.add("Div__dropDown")
+
+            let dropDownButton = document.createElement("button")
+            dropDownButton.classList.add("Div__dropDown-button")
+
+            let dropDownIcon = document.createElement("i")
+            dropDownIcon.classList.add("fas", "fa-chevron-down")
+
+            main.appendChild(section);
+            section.appendChild(article);
+            article.appendChild(divSectionArticleDiv);
+            article.appendChild(div);
+            div.appendChild(headLine);
+            headLine.appendChild(frontPageNews);
+            article.appendChild(dropDownDiv);
+            dropDownDiv.appendChild(dropDownButton);
+            dropDownButton.appendChild(dropDownIcon);
+        });
     });
 
 
-
-});
-
-
-/*
+/* const newsSection = document.querySelector(".Main")
 newsSection.innerHTML += `
             <section class="Main__section Section">
         <article class="Section__article">
@@ -33,4 +66,5 @@ newsSection.innerHTML += `
             </div>
         </article>
     </section>
-        ` */
+        `
+ */
